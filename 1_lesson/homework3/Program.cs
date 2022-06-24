f9cc433eff59aee1ab1c3f77c85d895fc5dfcec5
Katarina <novikkatarina@gmail.com>
2022-06-24 21:48:28 +0400

    hw3

---
+++ 1_lesson/homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
