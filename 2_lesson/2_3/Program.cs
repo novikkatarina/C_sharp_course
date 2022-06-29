bf9ff1868c11cf49341c23f379d5c20cd3cd4812
Katarina <novikkatarina@gmail.com>
2022-06-29 22:34:03 +0400

    task1 2 3 4

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int num2 = int.Parse(Console.ReadLine());
+
+void OneOfNum (int num1, int num2)
+{
+    if (num2 % num1 == 0)
+    {
+        Console.WriteLine("Multiple");
+    }
+    else
+    {
+        Console.WriteLine($"{num1}, {num2} -> not multiple, the remainder {num2 % num1}");
+    }
+}
+OneOfNum(num1,num2);
+// 3. Напишите программу, которая будет принимать 
+// на вход два числа и выводить,
+//    является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то
+//    программа выводит остаток от деление.
---
+++ 2_lesson/2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+
+void OneOfNum (int num1)
+{
+    if (num % 7 == 0 && num1 % 23 == 0)
+    {
+        Console.WriteLine("Multiple");
+    }
+    else
+    {
+        Console.WriteLine("not multiple");
+    }
+}
+OneOfNum(num);
