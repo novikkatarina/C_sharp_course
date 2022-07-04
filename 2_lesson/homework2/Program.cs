c79445dee1c5f39de1bd0d71afd059eb5c105d60
Katarina <novikkatarina@gmail.com>
2022-07-04 10:21:35 +0400

    hw3

--- 2_lesson/homework2/Program.cs
+++ 2_lesson/homework2/Program.cs
@@ -7,9 +7,13 @@ if (num < 100)
     Console.WriteLine("третьей цифры нет");
     return;
 }
-
-int num3 = num % 1000;
-Console.WriteLine($"{num3}");
+void ThirdOfNum (int num)
+{
+    int num321 = num % 1000;
+    int num3 = num321 / 100;
+    Console.WriteLine($"{num3}"); 
+}
+ThirdOfNum (num);
 
 
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
@@ -19,16 +23,3 @@ Console.WriteLine($"{num3}");
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
-
-
-// while ((num > 999) || (num<100));
-
-// // :10  = 
-
-// void SecOfNum (int num)
-// {
-//     int num23 = num % 100;
-//     int num2 = num23 / 10;
-//     Console.WriteLine($"the second number {num2}");
-// }
-// SecOfNum(num);
