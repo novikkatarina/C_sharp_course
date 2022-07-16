bb30e1ab626fba0376ffecb315f6750302334a65
Katarina <novikkatarina@gmail.com>
2022-07-16 15:29:03 +0400

    les5_1

---
+++ 5_lesson/5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами от [-9, 9]
+
+//Сумма отрицательных и положительных чисел
+
+int[] ArrayFill(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void ArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SumMaxMin(int[] array)
+{
+    int s_max, s_min;
+    s_max = s_min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0) s_max += array[i];
+        else s_min += array[i];
+    }
+    Console.Write($"sum max: {s_max}, sum min: {s_min} ");
+}
+int[] array = ArrayFill(12);
+ArrayPrint(array);
+SumMaxMin(array);
